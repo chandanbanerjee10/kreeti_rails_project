4f8408c0f77f3aeae7f0963a437b68543a19764d
chandanbanerjee10 <chandanbanerjee514@gmail.com>
2022-05-19 23:28:53 +0530

    Madew some changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_19_112945) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_19_170432) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -50,6 +50,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_19_112945) do
     t.integer "user_id"
   end
 
+  create_table "roles", force: :cascade do |t|
+    t.boolean "candidate"
+    t.boolean "recruiter"
+    t.boolean "admin"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
@@ -57,8 +65,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_19_112945) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "role"
-    t.string "uid"
-    t.string "provider"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
