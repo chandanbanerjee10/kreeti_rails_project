959b6bd83d5d715463c97c0c36259ec6e8a633ca
chandanbanerjee10 <chandanbanerjee514@gmail.com>
2022-06-01 14:16:56 +0530

    messages styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,6 +22,17 @@ class ApplicationController < ActionController::Base
         end
     end
 
+    def require_candidate
+        if !logged_in? 
+            flash[:alert] = "You must be logged in to perform that action"
+            redirect_to login_path
+        elsif current_user.role != "candidate"
+            session[:user_id] = nil
+            flash[:alert] = "You must be a candidate to perform that action"
+            redirect_to login_path
+        end
+    end
+
     def require_admin_recruiter
         if current_user.role == "candidate"
             flash[:alert] = "You must be a recruiter or an admin to perform that action"
